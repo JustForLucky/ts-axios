74238e5ce121878c9759e857735a26a4f0d6b7a5
lili <751502445@qq.com>
2020-09-19 20:27:29 +0800

    [build] 0.0.1

--- release.sh
+++ release.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env sh
-# set -e
-# echo "Enter release version"
-# read VERSION
-# read -p "Releasing $VERSION - are you sure?(y/n)" -n 1 -r
-# echo #(optional) move to a new line
-# if [[ $REPLY =~ [Yy]$ ]]
-# then
-#   echo "Releasing $VERSION"
+!/usr/bin/env sh
+set -e
+echo "Enter release version"
+read VERSION
+read -p "Releasing $VERSION - are you sure?(y/n)" -n 1 -r
+echo #(optional) move to a new line
+if [[ $REPLY =~ [Yy]$ ]]
+then
+  echo "Releasing $VERSION"
 
   #commit
   git add -A
@@ -16,4 +16,4 @@
 
   #publish
   npm publish
-# fi
+fi
